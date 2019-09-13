19f618aa46b586261ea69a4bff5f05de41d41267
tawnyblvd <margara.sees@gmail.com>
2019-09-12 21:44:52 -0400

    update for arr q3

---
+++ arrays1Exercises/makePi.js
@@ -0,0 +1,9 @@
+//Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+function makePi() {
+  let piArr = [];
+  piArr.push(3,1,4);
+  return piArr;
+}
+
+console.log(makePi());
+//3,1,4
