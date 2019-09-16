cd8219d251da442a8d194de6124e4b3cc02cf9b5
tawnyblvd <margara.sees@gmail.com>
2019-09-15 20:44:38 -0400

    more warmup exercise work

---
+++ warmup1Exercises/makes10.js
@@ -0,0 +1,10 @@
+//Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10
+function makes10(a, b){
+  if ((a+b === 10) {
+    return true;
+  } else if (a === 10 || b === 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
